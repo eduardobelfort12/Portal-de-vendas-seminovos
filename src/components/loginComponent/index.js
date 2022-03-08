7de3d48dd973354d4904d2cee38c31db0e3ba0d9
eduardobelfort12 <eduardoandrade1916@gmail.com>
2022-03-08 13:09:57 -0300

    formulário cadastro de veiculos

--- src/components/loginComponent/index.js
+++ src/components/loginComponent/index.js
@@ -9,6 +9,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import EmailIcon from "@material-ui/icons/Email";
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: "100vw",
