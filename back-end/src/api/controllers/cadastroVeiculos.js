a91ff8f41696b94d90b3347babd8e397500d42af
eduardobelfort12 <eduardoandrade1916@gmail.com>
2022-03-09 14:18:23 -0300

    configurando requisição com axios

--- back-end/src/api/controllers/cadastroVeiculos.js
+++ back-end/src/api/controllers/cadastroVeiculos.js
@@ -2,7 +2,8 @@ const knex = require('../models/databaseConnect')
 
 //controller para criação de registro para cadastro de veiculos no portal de vendas 
 const cadastroVeiculosController = async (req, res) => {
-    const {telefone,preco,potencia,torque,km,cor,cabine,
+    const {
+        telefone,preco,potencia,torque,km,cor,cabine,
         tiposuspensao,relacaodiferencial,entreeixo,
         capacidadecombustivel,opcionais,informacoesadicionais} = req.body
    await knex('cadastro_veiculos').insert({
