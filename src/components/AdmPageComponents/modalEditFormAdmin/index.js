e96fb1cf9f2c8b8ce8db7310da2cb871a9d66de9
eduardobelfort12 <eduardoandrade1916@gmail.com>
2022-03-30 14:21:45 -0300

    Trabalhando na edição dos itens dos anúncios do painel do administrador

---
+++ src/components/AdmPageComponents/modalEditFormAdmin/index.js
@@ -0,0 +1,74 @@
+import React, {} from "react";
+import { ContactMail } from "@material-ui/icons";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+// import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+// import EmailIcon from "@material-ui/icons/Email";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+    maxWidth: "100vw",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    
+  },
+  paper: {
+    margin: theme.spacing(9, 5),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent:"center"
+  },
+  form: {
+    // Fix IE 11 issue.
+   
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    background: "#006640",
+    color: "white",
+  },
+  icon: {
+    width: "50vw",
+    height: "50px",
+  },
+}));
+
+export default function EditFormAdmin() {
+  const classes = useStyles();
+
+  
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid alignContent="center" item xs={12} md={6} component={Paper} elevation={20} square>
+        <div className={classes.paper}>
+          <div>
+            <ContactMail className={classes.icon} />
+          </div>
+          <Typography component="h6" variant="h5"></Typography>
+          <form className={classes.form} noValidate>
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              className={classes.submit}
+            >
+                Confirmar Edição
+            </Button>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
