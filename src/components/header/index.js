d7d33754a8d55ed4f4ba5cded1b7b68511222546
eduardobelfort12 <eduardoandrade1916@gmail.com>
2022-04-14 14:24:53 -0300

    React-Adal implementation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import Login from "../loginComponent";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,15 +23,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function NavHeader() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
   return (
     <Navbar
       fixed="top"
@@ -64,31 +53,16 @@ export default function NavHeader() {
             <Link className={classes.root} to="/Estoque">
               <Nav.Link href="#Estoque">Estoque</Nav.Link>
             </Link>
-            <Link className={classes.root} to="/Contatos">
+            <Link className={classes.root} to="/Contato">
               <Nav.Link href="#Contato">Contatos</Nav.Link>
             </Link>
             <Link className={classes.root} to="/Login">
-              <Nav.Link onClick={handleOpen}>Login</Nav.Link>
+              <Nav.Link href="#Login">Login</Nav.Link>
             </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
-      <Modal
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 1000,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <Login />
-          </div>
-        </Fade>
-      </Modal>
     </Navbar>
   );
+  
 }
