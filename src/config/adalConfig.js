60ada08724de5ef59969bb4bedf4f2cdc7c6ee48
Eduardo Belfort <54038093+eduardobelfort12@users.noreply.github.com>
2023-02-09 13:30:47 -0300

    Update adalConfig.js

--- src/config/adalConfig.js
+++ src/config/adalConfig.js
@@ -1,11 +1,11 @@
 import { AuthenticationContext, adalFetch, withAdalLogin } from 'react-adal';
 
 export const adalConfig = {
-  tenant: '56eda749-b95e-4a74-93de-f01fd6b161cc',
-  clientId: '081347b0-56f5-414b-9f25-8142fdb4b12c',
+  tenant: '..................................',
+  clientId: '......................................',
   redirectUri: "http://localhost:3000/AdminPainel",
   endpoints: {
-    api: '56eda749-b95e-4a74-93de-f01fd6b161cc',
+    api: '........................................',
   },
   
   cacheLocation: 'localStorage',
